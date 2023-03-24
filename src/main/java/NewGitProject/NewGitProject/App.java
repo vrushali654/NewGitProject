6e0c76d918b70cfea13a790b4ba12e57c6fbb7e6
HP <HP@DESKTOP-2GALD3K>
2023-03-24 12:06:53 +0530

    First test pass

---
+++ src/main/java/NewGitProject/NewGitProject/App.java
@@ -0,0 +1,13 @@
+package NewGitProject.NewGitProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
